401da698d55220fbd7bb86ce14de771d24070acc
Vanadiry <vanadiry@yeah.net>
2022-04-18 14:52:13 +0800

    23

---
+++ library/stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?295a33a31d3555dabfe9c1da347a8c2d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
